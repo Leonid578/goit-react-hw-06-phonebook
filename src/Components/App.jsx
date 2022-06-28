780408ffc27db06e1137d66dd630f2e5a0f3a44c
Leonid578 <76591317+Leonid578@users.noreply.github.com>
2022-06-28 22:45:23 +0300

    2

---
+++ src/Components/App.jsx
@@ -0,0 +1,42 @@
+import {Form} from './Form/Form';
+import Title from './Title/Title';
+import style from './App.module.css';
+import ContactList from './ContactList/ContactList';
+import NotContacts from './NotContacts/NotContacts';
+import { useEffect } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { apdateInitialContacts } from 'redux/sliceContacts';
+
+const App = () => {
+  const listContacts = useSelector(state => state.items);
+  const dispatch = useDispatch()
+
+  
+  // тут я работаю с жизнеными циклами и проверяю локалку при рендере страницы
+  useEffect(() => {
+    const lCont = localStorage.getItem('contacts');
+    if (lCont) {
+      dispatch(apdateInitialContacts(JSON.parse(lCont)));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(listContacts));
+  }, [listContacts]);
+
+  return (
+    <div className={style.conteiner}>
+      <Title text={'Phonebook'} />
+      <Form/>
+      <Title text={'Contacts'} />
+
+      {listContacts.length < 1 ? (
+        <NotContacts text={'The contact list is currently empty'} />
+      ) : (
+        <ContactList/>
+      )}
+    </div>
+  );
+};
+export default App;
